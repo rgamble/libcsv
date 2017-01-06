b4f84333e1dcd03c956e25292191059cfde0d232
bobhairgrove <code@roberthairgrove.com>
2017-01-06 20:57:35 +0100

    Added #include <assert.h> to libcsv.c

--- libcsv.c
+++ libcsv.c
@@ -17,6 +17,8 @@ License along with this library; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
+#include <assert.h>
+
 #if __STDC_VERSION__ >= 199901L
 #  include <stdint.h>
 #else
